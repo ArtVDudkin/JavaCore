e7440506f1191028246613ed8dc5a2a41c81feb9
Artem <kardan97@yandex.ru>
2023-06-15 21:05:10 +0700

    Some editions

--- Hometask4/Hometask4/src/main/java/ru/geekbrains/hometask4/MyArraySizeException.java
+++ Hometask4/Hometask4/src/main/java/ru/geekbrains/hometask4/MyArraySizeException.java
@@ -5,6 +5,7 @@ public class MyArraySizeException extends RuntimeException {
     private String message;
     private int maxRow;
     private int maxCol;
+
     public MyArraySizeException(String message, int maxRow, int maxCol) {
         this.message = message;
         this.maxRow = maxRow;
